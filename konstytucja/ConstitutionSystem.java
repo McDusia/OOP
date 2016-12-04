1d80a388b28ff8d4e14d2aaf9639a46f8a6892bf
McDusia <madzia.nowak222@poczta.fm>
2016-12-04 17:50:57 +0100

    final version

--- konstytucja/ConstitutionSystem.java
+++ konstytucja/ConstitutionSystem.java
@@ -4,15 +4,10 @@ package konstytucja;
 public class ConstitutionSystem {
 	
 	
-	
 	public static void main (String[] args)
 	{
 		ConstitutionParser parser = new ConstitutionParser();
 		Constitution constitution = parser.load(args[0]);
-		
-		//constitution.joinDividedWords();
-		//constitution.deleteDates();
-		
 		
 		switch (args[1])
 		{
@@ -27,7 +22,6 @@ public class ConstitutionSystem {
 		return;
 			
 	}
-	
 	
 }
 	
