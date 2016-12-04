bcac93107351ed65cc7c536f2f47fbfc10e51945
McDusia <madzia.nowak222@poczta.fm>
2016-12-04 17:47:59 +0100

    without empty functions

--- konstytucja/Chapter.java
+++ konstytucja/Chapter.java
@@ -31,22 +31,11 @@ List <Section> sections;
 		return null;
 	}
 	
-	
-	public void joinDividedWords()
-	{
-		// usuwa z koñca linii znaki przeniesienia do nowej liniii (-) i ³¹czy³ s³owa w ca³oœæ,
-		// Utworzy now¹ zawartoœæ listy introduction
-		// Nastêpnie zrobi to samo dla wszystkich rozdzia³ów, sekcji i artyku³ów.
-	}
-	public void deleteDates()
-	{
-	}
 	public String toString()
 	{
 		String wynik = title+"\r";
 		for(int i=0;i<sections.size();i++)
 		{
-			
 			wynik += sections.get(i).toString()+"\r";
 		}
 		return wynik;
